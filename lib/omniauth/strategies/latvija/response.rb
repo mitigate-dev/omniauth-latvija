ef67f01f9d2c7c593c76e87ebca8a214a5c8708e
Kristaps Kulikovskis <kristaps.kulikovskis@makit.lv>
2017-10-20 00:53:53 +0300

    WIP add decryption

--- lib/omniauth/strategies/latvija/response.rb
+++ lib/omniauth/strategies/latvija/response.rb
@@ -1,53 +1,51 @@
-module OmniAuth
-  module Strategies
-    class Latvija
-      class Response
-        ASSERTION = "urn:oasis:names:tc:SAML:1.0:assertion"
+module OmniAuth::Strategies
+  class Latvija
+    class Response
+      ASSERTION = 'urn:oasis:names:tc:SAML:1.0:assertion'
 
-        attr_accessor :options, :response, :document
+      attr_accessor :options, :response, :document
 
-        def initialize(response, options = {})
-          raise ArgumentError.new("Response cannot be nil") if response.nil?
-          self.options  = options
-          self.response = response
-          self.document = OmniAuth::Strategies::Latvija::SignedDocument.new(response)
+      def initialize(response, **options)
+        raise ArgumentError.new('Response cannot be nil') if response.nil?
+        @options  = options
+        @response = response
+        @document = OmniAuth::Strategies::Latvija::SignedDocument.new(response, private_key: options[:private_key])
+      end
+
+      def validate!
+        document.validate!(fingerprint)
+      end
+
+      def authentication_method
+        @authentication_method ||= begin
+          REXML::XPath.first(document, "//saml:AuthenticationStatement").attributes['AuthenticationMethod']
         end
+      end
 
-        def validate!
-          document.validate!(fingerprint)
-        end
+      # A hash of alle the attributes with the response. Assuming there is only one value for each key
+      def attributes
+        @attributes ||= begin
+          result = {}
 
-        def authentication_method
-          @authentication_method ||= begin
-            REXML::XPath.first(document, "//saml:AuthenticationStatement").attributes['AuthenticationMethod']
+          stmt_element = REXML::XPath.first(document, "//a:Assertion/a:AttributeStatement", { "a" => ASSERTION })
+          return {} if stmt_element.nil?
+
+          stmt_element.elements.each do |attr_element|
+            name  = attr_element.attributes["AttributeName"]
+            value = attr_element.elements.first.text
+
+            result[name] = value
           end
+
+          result
         end
+      end
 
-        # A hash of alle the attributes with the response. Assuming there is only one value for each key
-        def attributes
-          @attributes ||= begin
-            result = {}
+      private
 
-            stmt_element = REXML::XPath.first(document, "//a:Assertion/a:AttributeStatement", { "a" => ASSERTION })
-            return {} if stmt_element.nil?
-
-            stmt_element.elements.each do |attr_element|
-              name  = attr_element.attributes["AttributeName"]
-              value = attr_element.elements.first.text
-
-              result[name] = value
-            end
-
-            result
-          end
-        end
-
-        private
-
-        def fingerprint
-          cert = OpenSSL::X509::Certificate.new(options[:certificate])
-          Digest::SHA1.hexdigest(cert.to_der).upcase.scan(/../).join(":")
-        end
+      def fingerprint
+        cert = OpenSSL::X509::Certificate.new(options[:certificate])
+        Digest::SHA1.hexdigest(cert.to_der).upcase.scan(/../).join(":")
       end
     end
   end
--- lib/omniauth/strategies/latvija/signed_document.rb
+++ lib/omniauth/strategies/latvija/signed_document.rb
@@ -15,80 +15,75 @@
 # If applicable, add the following below the CDDL Header,
 # with the fields enclosed by brackets [] replaced by
 # your own identifying information:
-# "Portions Copyrighted [year] [name of copyright owner]"
+# 'Portions Copyrighted [year] [name of copyright owner]'
 #
 # $Id: xml_sec.rb,v 1.6 2007/10/24 00:28:41 todddd Exp $
 #
 # Copyright 2007 Sun Microsystems Inc. All Rights Reserved
 # Portions Copyrighted 2007 Todd W Saxton.
 
-module OmniAuth
-  module Strategies
-    class Latvija
-      class SignedDocument < REXML::Document
-        DSIG = "http://www.w3.org/2000/09/xmldsig#"
+module OmniAuth::Strategies
+  class Latvija
+    class SignedDocument
+      DSIG = 'http://www.w3.org/2000/09/xmldsig#'.freeze
+      XENC = 'http://www.w3.org/2001/04/xmlenc#'.freeze
 
-        attr_accessor :signed_element_id
+      attr_accessor :signed_element_id
 
-        def initialize(response)
-          super(response)
-          extract_signed_element_id
+      def initialize(response, **opts)
+        @response = Nokogiri::XML(response)
+        if encrypted?
+          decryptor = OmniAuth::Strategies::Latvija::Decryptor.new(response, opts[:private_key])
+          decrypted_response = decryptor.decrypt
+          @response = Nokogiri::XML(decrypted_response)
         end
 
-        def validate!(idp_cert_fingerprint)
-          # get cert from response
-          base64_cert = self.elements["//ds:X509Certificate"].text
-          cert_text   = Base64.decode64(base64_cert)
-          cert        = OpenSSL::X509::Certificate.new(cert_text)
+        extract_signed_element_id
+      end
 
-          # check cert matches registered idp cert
-          fingerprint = Digest::SHA1.hexdigest(cert.to_der)
+      def validate!(idp_cert_fingerprint)
+        # get cert from response
+        base64_cert = @response.xpath('//xmlns:X509Certificate', xmlns: DSIG).text
+        cert_text   = Base64.decode64(base64_cert)
+        cert        = OpenSSL::X509::Certificate.new(cert_text)
 
-          if fingerprint != idp_cert_fingerprint.gsub(/[^a-zA-Z0-9]/,"").downcase
-            raise ValidationError.new("Fingerprint mismatch")
-          end
+        # check cert matches registered idp cert
+        fingerprint = Digest::SHA1.hexdigest(cert.to_der)
+        raise ValidationError, 'Fingerprint mismatch' if fingerprint != idp_cert_fingerprint.gsub(/[^a-zA-Z0-9]/, '').downcase
 
-          # remove signature node
-          sig_element = REXML::XPath.first(self, "//ds:Signature", { "ds" => DSIG })
-          sig_element.remove
+        # remove signature node
+        sig_element = @response.xpath('//xmlns:Signature', xmlns: DSIG)
+        sig_element.remove
 
-          # check digests
-          REXML::XPath.each(sig_element, "//ds:Reference", {"ds"=>"http://www.w3.org/2000/09/xmldsig#"}) do |ref|
-            uri                           = ref.attributes.get_attribute("URI").value
-            hashed_element                = REXML::XPath.first(self, "//[@AssertionID='#{uri[1,uri.size]}']")
-            canoner                       = XML::Util::XmlCanonicalizer.new(false, true)
-            canon_hashed_element          = canoner.canonicalize(hashed_element)
-            hash                          = Base64.encode64(Digest::SHA1.digest(canon_hashed_element)).chomp
-            digest_value                  = REXML::XPath.first(ref, "//ds:DigestValue", { "ds" => DSIG }).text
+        # check digests
+        sig_element.xpath('.//xmlns:Reference', xmlns: DSIG).each do |ref|
+          uri            = ref.attribute('URI').value
+          hashed_element = @response.xpath("//*[@AssertionID='#{uri[1, uri.size]}']").canonicalize
+          hash           = Base64.encode64(Digest::SHA1.digest(hashed_element)).chomp
+          digest_value   = ref.xpath('.//xmlns:DigestValue', xmlns: DSIG).text
 
-            if hash != digest_value
-              raise ValidationError.new("Digest mismatch")
-            end
-          end
-
-          # verify signature
-          canoner                 = XML::Util::XmlCanonicalizer.new(false, true)
-          signed_info_element     = REXML::XPath.first(sig_element, "//ds:SignedInfo", { "ds" => DSIG })
-          canon_string            = canoner.canonicalize(signed_info_element)
-
-          base64_signature        = REXML::XPath.first(sig_element, "//ds:SignatureValue", { "ds" => DSIG }).text
-          signature               = Base64.decode64(base64_signature)
-
-          # get certificate object
-          cert_text               = Base64.decode64(base64_cert)
-          cert                    = OpenSSL::X509::Certificate.new(cert_text)
-
-          if !cert.public_key.verify(OpenSSL::Digest::SHA1.new, signature, canon_string)
-            raise ValidationError.new("Key validation error")
-          end
-
-          true
+          raise ValidationError, 'Digest mismatch' if hash != digest_value
         end
 
-        def extract_signed_element_id
-          reference_element       = REXML::XPath.first(self, "//ds:Signature/ds:SignedInfo/ds:Reference", { "ds" => DSIG })
-          self.signed_element_id  = reference_element.attribute("URI").value unless reference_element.nil?
+        # verify signature
+        signed_info_element = Nokogiri::XML(sig_element.xpath('.//xmlns:SignedInfo', xmlns: DSIG).to_s).canonicalize
+        base64_signature    = sig_element.xpath('.//xmlns:SignatureValue', xmlns: DSIG).text
+        signature           = Base64.decode64(base64_signature)
+
+        unless cert.public_key.verify(OpenSSL::Digest::SHA1.new, signature, signed_info_element)
+          raise ValidationError, 'Key validation error'
         end
+
+        true
+      end
+
+      def extract_signed_element_id
+        reference_element       = @response.xpath('//ds:Signature/ds:SignedInfo/ds:Reference', 'ds' => DSIG)
+        self.signed_element_id  = reference_element.attribute('URI').value unless reference_element.nil?
+      end
+
+      def encrypted?
+        @response.xpath('//xenc:EncryptedData', 'xmlns:xenc' => XENC).any?
       end
     end
   end
